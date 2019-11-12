0658eca4a8bdcca5699bd9f5d024cfabb2120f30
Odin <sergius.la@gmail.com>
2019-11-12 08:31:44 -0800

    remove_elements

---
+++ src/main/java/arrays/in_place/RemoveElement.java
@@ -0,0 +1,27 @@
+package arrays.in_place;
+
+public class RemoveElement {
+    /**
+     *
+     * Given an array nums and a value val, remove all instances of that value in-place and return the new length.
+     * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+     * The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+     *
+     * Example:
+     * Given nums = [3,2,2,3], val = 3,
+     * Your function should return length = 2, with the first two elements of nums being 2.
+     * It doesn't matter what you leave beyond the returned length.
+     *
+     * [LeetCode] - https://leetcode.com/problems/remove-element/description/
+    * */
+    public int removeElement(int[] nums, int val) {
+        int resIndex = 0;
+        for (int i = 0; i <= nums.length - 1; i++) {
+            if (nums[i] != val) {
+                nums[resIndex] = nums[i];
+                resIndex++;
+            }
+        }
+        return resIndex;
+    }
+}
