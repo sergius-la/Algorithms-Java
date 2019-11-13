62a55e5ddbbc5468dba81e6eeaed38324a09bfdb
Odin <sergius.la@gmail.com>
2019-11-13 08:50:36 -0800

    DetectCapital

---
+++ src/main/java/string_manipulation/DetectCapital.java
@@ -0,0 +1,60 @@
+package string_manipulation;
+
+import org.testng.Assert;
+
+public class DetectCapital {
+
+    public static void main(String[] args) {
+        DetectCapital detectCapital = new DetectCapital();
+        Assert.assertEquals(detectCapital.detectCapitalUse("USA"), true);
+        Assert.assertEquals(detectCapital.detectCapitalUse("FlaG"), false);
+        Assert.assertEquals(detectCapital.detectCapitalUse("leetcode"), true);
+        Assert.assertEquals(detectCapital.detectCapitalUse("Leetcode"), true);
+    }
+
+    /**
+     * Given a word, you need to judge whether the usage of capitals in it is right or not.
+     *
+     * We define the usage of capitals in a word to be right when one of the following cases holds:
+     *
+     * All letters in this word are capitals, like "USA".
+     * All letters in this word are not capitals, like "leetcode".
+     * Only the first letter in this word is capital, like "Google".
+     * Otherwise, we define that this word doesn't use capitals in a right way.
+     *
+     *
+     * Example 1:
+     * Input: "USA"
+     * Output: True
+     *
+     *
+     * Example 2:
+     * Input: "FlaG"
+     * Output: False
+     *
+     * Challenge - https://leetcode.com/problems/detect-capital/
+    * */
+    public boolean detectCapitalUse(String word) {
+        int numUpperCase = 0;
+        for (Character current: word.toCharArray()) {
+            if (isUpperCase(current)) {
+                numUpperCase++;
+            }
+        }
+        boolean firstCapital = (word.length() > 0 && isUpperCase(word.charAt(0)) && numUpperCase == 1);
+        if (numUpperCase == word.length() && word.length() > 0) {
+            return true;
+        } else if (firstCapital) {
+            return true;
+        } else if (numUpperCase == 0 && word.length() > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    private boolean isUpperCase(Character character) {
+        String upper = character.toString().toUpperCase();
+        return upper.equals(character.toString());
+    }
+}
